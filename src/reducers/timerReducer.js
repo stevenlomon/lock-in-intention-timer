39ef029a6bb13428d7da5016f04d55646cfa5072
StevenLomon <steven.cesario@proton.me>
2026-06-23 09:50:27 +0200

    timerReducer fully implemented

--- src/reducers/timerReducer.js
+++ src/reducers/timerReducer.js
@@ -1,5 +1,5 @@
-// My original vanilla JS made it possible for an invalid state if both `isRunning` and `intentionEndScreen` are both true!'
-// By using strings we make it physically impossible for them to overlap into invalid states
+// My original vanilla JS version made it possible for an invalid state if both `isRunning` and `intentionEndScreen` would somehow both end up at true!
+// By using strings we make it mathematically impossible for them to overlap into invalid states
 export const APP_STATES = {
   START: 'START',
   RUNNING: 'RUNNING',
@@ -17,19 +17,58 @@ function timerReducer(state, action) {
   switch (action.type) {
     case 'START_TIMER':
       // Only allow starting if we are currently at START or PAUSED
-      if (state.status === APP_STATES.START || state.stauts === APP_STATES.PAUSED) {
+      if (state.status === APP_STATES.START || state.status === APP_STATES.PAUSED) {
         return {
           ...state, // Spread the current state, update keys where needed
           status: APP_STATES.RUNNING, // RUNNING is the new state
           totalSeconds: action.payload !== undefined
-            ? action.payload      // If the action includes validated seconds different from the default 2700, use them!
-            : state.totalSeconds  // Else, use our default
+            ? action.payload     // If the action includes validated seconds different from the default 2700, use them!
+            : state.totalSeconds // Else, use our default
         };
       }
       return state; // If the action is called and it would lead to an invalid state (we're already running or at the end screen), we ignore it
 
+    case 'PAUSE_TIMER':
+      // Only allow pausing if we are currently at RUNNING
+      if (state.status === APP_STATES.RUNNING) {
+        return {
+          ...state,
+          status: APP_STATES.PAUSED, // Only updates the app states
+        };
+      }
+      return state;
+
+    case 'TICK':
+      // Once again: Only allow ticking if we are currently at RUNNING
+      if (state.status === APP_STATES.RUNNING) {
+        return {
+          ...state,
+          totalSeconds: action.payload, // Only updates the seconds amount
+        };
+      }
+      return state;
+
+    case 'FINISH_TIMER':
+      // We only allow the timer to finish if it is currently at RUNNING too
+      if (state.status === APP_STATES.RUNNING) {
+        return {
+          ...state,
+          status: APP_STATES.END, // End state
+          totalSeconds: 0         // Seconds is explicitly set to zero
+        };
+      }
+      return state;
+
+    case 'RESET_TIMER':
+      // We only allow the timer to reset if is currently at RUNNING, PAUSED or END. You can't reset a timer that hasn't started yet
+      if (state.status !== APP_STATES.START) {
+        // We simply return the initial state!
+        return initialState;
+      }
+      return state;
+
     default:
       // If an unknown action is sent, don't crash, just do nothing.
       return state;
-  }
-}
+  };
+};
