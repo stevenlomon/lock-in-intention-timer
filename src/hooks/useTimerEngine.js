2f598a5b7bd09eb823a8cadb9e1619be0faf4b8f
StevenLomon <steven.cesario@proton.me>
2026-06-23 10:08:23 +0200

    timerReducer integrated into useTimerEngine

--- src/hooks/useTimerEngine.js
+++ src/hooks/useTimerEngine.js
@@ -1,16 +1,22 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useReducer } from 'react';
+import { APP_STATES, timerReducer } from "./../reducers/timerReducer"; 
 import { StorageManager } from "./../utils/StorageManager";
 import { AudioEngine } from "./../utils/AudioEngine";
 
 export function useTimerEngine(initialSeconds = 2700) { // Using our default 45 minutes
-  const [totalSeconds, setTotalSeconds] = useState(initialSeconds);
-  const [isRunning, setIsRunning] = useState(false);
-  const [isEndScreen, setIsEndScreen] = useState(false);
+  // const [totalSeconds, setTotalSeconds] = useState(initialSeconds);
+  // const [isRunning, setIsRunning] = useState(false);
+  // const [isEndScreen, setIsEndScreen] = useState(false);
+  // These three state variables are now replaced with our new reducer handling the state!
+  const [state, dispatch] = useReducer(timerReducer, {
+    status: APP_STATES.START,
+    totalSeconds: initialSeconds
+  });
 
   // Our "Heartbeat" is now a useEffect!
   useEffect(() => {
     // Guard Clause: if the app is *not* running, do nothing. Return early
-    if (!isRunning) return;
+    if (state.status !== APP_STATES.RUNNING) return;
 
     // If we *are* running, start the interval
     const interval = setInterval(() => {
@@ -19,17 +25,14 @@ export function useTimerEngine(initialSeconds = 2700) { // Using our default 45 
 
     }, 1000); // Run this logic every second that the timer is ticking
 
-    // Cleanup: Function reference to be run automatically by React when isRunning becomes false
+    // Cleanup: Function reference to be run automatically by React when the state status changes
     return () => clearInterval(interval);
 
-  }, [isRunning]); // Only run this when isRunning changes
+  }, [state.status]); // Only run this when the state status changes
 
-  // Our start, stop, pause and reset methods, now as functions
+  // Our start, pause and reset methods, now as functions
+  // And now furthermore with our reducer, functions that call dispatch to the reducer! The reducer defines the actions, here's where we define the functions that use the actions
   const start = (validatedSeconds) => {
-    // To be implemented
-  };
-
-  const stop = () => {
     // To be implemented
   };
 
@@ -46,11 +49,9 @@ export function useTimerEngine(initialSeconds = 2700) { // Using our default 45 
 
   // Return the state and control functions for our components to use
   return {
-    totalSeconds,
-    isRunning,
-    isEndScreen,
+    status: state.status,
+    totalSeconds: state.totalSeconds,
     start,
-    stop,
     pause,
     reset
   }
