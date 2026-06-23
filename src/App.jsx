411cebcc8dced39def1221a61e9addfc51c7a235
StevenLomon <steven.cesario@proton.me>
2026-06-23 10:33:43 +0200

    app state integrated into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,38 @@ import './App.css';
 import TimeDisplay from "./components/TimeDisplay";
 import IntentionInput from './components/IntentionInput';
 import Controls from "./components/Controls";
+import { useTimerEngine } from './hooks/useTimerEngine';
+import { APP_STATES } from './reducers/timerReducer';
 
 function App() {
-  
+  const { status } = useTimerEngine();
 
   return (
     <div className='timer-container'>
       <TimeDisplay />
-      <IntentionInput />
-      <Controls />
+
+      <div className='intention-container'>
+        <label className='intention-prompt'>
+          {status === APP_STATES.START
+            ? 'What is your intention for these low-distraction focus minutes?'
+            : status === APP_STATES.RUNNING || status === APP_STATES.PAUSED
+              ? 'Good Luck! 🌱✨'
+              : (
+                <>
+                  The minutes have passed.<br />
+                  Did you complete the intention you set?<br />
+                  Take a few moments to reflect on your estimation ability.<br />
+                  It's all feedback for growth and self discovery 🌱
+                </>
+              )
+          }
+        </label>
+        <IntentionInput />
+      </div>
+
+      <div className='controls-container'>
+        <Controls />
+      </div>
     </div>
   )
 }
