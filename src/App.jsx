b67df632ba3c81a0e554db0187f5afd2f4b6c7d7
StevenLomon <steven.cesario@proton.me>
2026-06-23 11:47:05 +0200

    TimeDusplay is now a  controlled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState } from 'react';
 import TimeDisplay from "./components/TimeDisplay";
 import IntentionInput from './components/IntentionInput';
 import Controls from "./components/Controls";
@@ -6,11 +7,16 @@ import { useTimerEngine } from './hooks/useTimerEngine';
 import { APP_STATES } from './reducers/timerReducer';
 
 function App() {
-  const { status } = useTimerEngine();
+  const { status, totalSeconds, start, pause, reset } = useTimerEngine();
+
+  // For our controlled inputs
+  const [timerInput, setTimerInput] = useState("45:00");
+  const [intentionText, setIntentionText] = useState("");
 
   return (
     <div className='timer-container'>
-      <TimeDisplay />
+      <TimeDisplay timerValue={timerInput} onTimerEdit={setTimerInput} />
+      {/* For debugging purposes only: timer value is currently {timerInput} */}
 
       <div className='intention-container'>
         <label className='intention-prompt'>
@@ -18,7 +24,8 @@ function App() {
             ? 'What is your intention for these low-distraction focus minutes?'
             : status === APP_STATES.RUNNING || status === APP_STATES.PAUSED
               ? 'Good Luck! 🌱✨'
-              : (
+              : ( 
+                // If the code reaches here - if it's not START, not RUNNING and not PAUSED, we can safely say it's END
                 <>
                   The minutes have passed.<br />
                   Did you complete the intention you set?<br />
--- src/components/TimeDisplay.jsx
+++ src/components/TimeDisplay.jsx
@@ -1,9 +1,15 @@
-
-
-const TimeDisplay = () => {
+const TimeDisplay = ({ timerValue, onTimerEdit }) => {
   return (
-    <div className='time-display' contentEditable={true} spellCheck={false}>
-      45:00
+    <div
+      className='time-display'
+      contentEditable={true}
+      suppressContentEditableWarning={true} // Special React property to signal to React "Hey, I know we're touching the DOM here but I know what I'm doing"
+      spellCheck={false}
+      // onChange={(e) => onTimerEdit(e.target.value)} This that is ingrained in my muscle memory at this poitn would be valid.. if we had an input! But here we have a div
+      // onInput={(e) => onTimerEdit(e.currentTarget.textContent)} This... is technically valid but completely unusable due to the cursor flickering haha
+      onBlur={(e) => onTimerEdit(e.currentTarget.textContent)} // And so this is what we end up with! Only update timerInput when the user clicks away (blur) or hits Enter!
+    >
+      {timerValue ? timerValue : "45:00"}
     </div>
   )
 }
