036879902926b3d43d72a05d336d57ca8db0b714
StevenLomon <steven.cesario@proton.me>
2026-06-24 08:42:29 +0200

    majority of useTimerEngine written (if not all?)

--- src/hooks/useTimerEngine.js
+++ src/hooks/useTimerEngine.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useReducer } from 'react';
+import { useState, useEffect, useReducer, useRef } from 'react';
 import { APP_STATES, APP_ACTIONS, timerReducer } from "./../reducers/timerReducer"; 
 import { StorageManager } from "./../utils/StorageManager";
 import { AudioEngine } from "./../utils/AudioEngine";
@@ -13,6 +13,8 @@ export function useTimerEngine(initialSeconds = 2700) { // Using our default 45 
     totalSeconds: initialSeconds
   });
 
+  const endTimeRef = useRef(null); // Will be refered to both in the useEffect and the start function so it's used to solve a scope issue here
+
   // Our "Heartbeat" is now a useEffect!
   useEffect(() => {
     // Guard Clause: if the app is *not* running, do nothing. Return early
@@ -21,8 +23,23 @@ export function useTimerEngine(initialSeconds = 2700) { // Using our default 45 
     // If we *are* running, start the interval
     const interval = setInterval(() => {
 
-      // The timer logic with Date.now() goes here
+      // Every single second that the timer ticks down...
+      // `const msRemaining = StateBuffer.endTime - Date.now();` becomes...
+      const msRemaining = endTimeRef.current - Date.now();
 
+      // `StateBuffer.totalSeconds = Math.max(0, Math.ceil(msRemaining / 1000));` becomes...
+      const newSeconds = Math.max(0, Math.ceil(msRemaining / 1000));
+      dispatch({ type: APP_ACTIONS.TICK, payload: newSeconds }); // We're not allowed to mutate state like `state.totalSeconds = Math.max(0, Math.ceil(msRemaining / 1000));` in React! *In React, state is strictly immutable* State is updated only via state setters, which is dispatch in this scenario
+
+      StorageManager.save(StorageManager.SECONDS_KEY, state.totalSeconds);
+
+      // Our stop condition `if (StateBuffer.totalSeconds <= 0) {` translated!
+      if (newSeconds <= 0) { // We can't use state.totalSeconds here due to JavaScript closures! It's a "stale" value
+        dispatch( {type: APP_ACTIONS.FINISH_TIMER }); // Replaces 5 lines of code haha!
+        
+        StorageManager.clearSession();
+        AudioEngine.playDing(); // Stays exactly the same!
+      }
     }, 1000); // Run this logic every second that the timer is ticking
 
     // Cleanup: Function reference to be run automatically by React when the state status changes
@@ -33,7 +50,10 @@ export function useTimerEngine(initialSeconds = 2700) { // Using our default 45 
   // Our start, pause and reset methods, now as functions
   // And now furthermore with our reducer, functions that call dispatch to the reducer! The reducer defines the actions, here's where we define the functions that use the actions
   const start = (validatedSeconds) => {
-    // To be implemented
+    // The translation of this line of code from the Vanilla JS version `StateBuffer.endTime = Date.now() + (StateBuffer.totalSeconds * 1000);` becomes...
+    endTimeRef.current = Date.now() + validatedSeconds * 1000; // Absolute end time in milliseconds
+    
+    dispatch({ type: APP_ACTIONS.START_TIMER, payload: validatedSeconds });
   };
 
   const pause = () => {
