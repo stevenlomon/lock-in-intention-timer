942797ff15fd34092b4c1ce3d39fe383865b4d31
StevenLomon <steven.cesario@proton.me>
2026-06-25 14:22:22 +0200

    ux: if the intention text reaches two rows, the textarea is now auto-expanding

--- src/components/IntentionInput.jsx
+++ src/components/IntentionInput.jsx
@@ -1,15 +1,32 @@
+import { useRef } from "react";
 import { APP_STATES } from "../reducers/timerReducer";
 
 const IntentionInput = ({ appStatus, intentionValue, onIntentionEdit, onStart }) => {
+  const textareaRef = useRef(null); // A reference hook to the textarea so that we can "reach through and touch the acual HTML DOM node"
+
+  // Extending our onChange
+  const handleChange = (e) => {
+    // First; what we already had
+    onIntentionEdit(e.target.value); // What we already had
+
+    // Second; the same DOM math from the Vanilla JS version, now using useRef
+    const textarea = textareaRef.current;
+    if (textarea) {
+      textarea.style.height = 'auto'; // Reset to allow shrinking
+      textarea.style.height = `${textarea.scrollHeight}px`; // Expand to fit text
+    }
+  }
+
   return (
     <textarea
+      ref={textareaRef}
       className="intention-input"
       placeholder="e.g., Complete the first 3 test exam exercises"
       autoComplete='off'
       spellCheck={false}
       rows={1}
       value={intentionValue}
-      onChange={(e) => onIntentionEdit(e.target.value)}
+      onChange={handleChange} // Use the expanded function
       disabled={appStatus === APP_STATES.START ? false : true}
 
       // Prevent 'Enter' from creating a newline and instead have it start the timer! (if valid intention input)
