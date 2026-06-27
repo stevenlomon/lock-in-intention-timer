9ba432fd6fb388a998daa7e5618a596bafea7208
StevenLomon <steven.cesario@proton.me>
2026-06-27 10:41:49 +0200

    formatting for readability

--- src/components/TimeDisplay.jsx
+++ src/components/TimeDisplay.jsx
@@ -7,6 +7,7 @@ const TimeDisplay = ({ appStatus, timerValue, onTimerEdit }) => {
       contentEditable={appStatus === APP_STATES.START}
       suppressContentEditableWarning={true} // Special React property to signal to React "Hey, I know we're touching the DOM here but I know what I'm doing"
       spellCheck={false}
+      
       // onChange={(e) => onTimerEdit(e.target.value)} This that is ingrained in my muscle memory at this poitn would be valid.. if we had an input! But here we have a div
       // onInput={(e) => onTimerEdit(e.currentTarget.textContent)} This... is technically valid but completely unusable due to the cursor flickering haha
       onBlur={(e) => { // And so this is what we end up with! Only update timerInput when the user clicks away (blur) or hits Enter!
